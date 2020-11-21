1c3bc0bcac12da4985ab26c9e281cb3d8e5d0e05
Douglas Hawthorne <dfhawthorne@computer.org>
2020-11-21 21:54:52 +1100

    Enhancement to mount_over_ssh.sh 1. Will not try to remount existing mounts 2. Shows what host caused the error 3. Added design journal entry 4. Updated design journal TOC

--- mount_over_ssh.sh
+++ mount_over_ssh.sh
@@ -9,6 +9,9 @@ hosts="auburn personal dural hammer"
 for host in ${hosts}
 do  mount_point="${HOME}/mount/${host}"
     [ -d "${mount_point}" ] || mkdir -p "${mount_point}"
-    sshfs "${host}.${domain}:${HOME}" "${mount_point}"
+    remote="${host}.${domain}:${HOME}"
+    findmnt "${mount_point}" >/dev/null || \
+        sshfs "${remote}" "${mount_point}"               || \
+        printf "Failed to mount %s\n" "${remote}" >&2
 done
 
