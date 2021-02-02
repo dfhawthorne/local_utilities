f2614427ba5a239a5f15eb88305d98df5c6c81ec
Douglas Hawthorne <dfhawthorne@computer.org>
2021-02-03 06:34:42 +1100

    Improved ansible-check.sh
    - Writes out correct error message for no playbook passed
    - Added comment blocks to sections of code
    - Replace `[` tests with `[[` ones
    - Creates a `.yamllint` file in the current directory if `yamllint` is installed
    - Populates `.yamllint` with my preferences

--- ansible-check.sh
+++ ansible-check.sh
@@ -7,17 +7,53 @@
 # 4. Execute the playbook in CHECK mode
 # ------------------------------------------------------------------------------
 
-if [ ! -f "$1" ]
+# ------------------------------------------------------------------------------
+# Validate passed arguments
+# ------------------------------------------------------------------------------
+
+if [[ $# -ne 1 ]]
+then
+    printf "No file provided\n" >&2
+    exit 1
+fi
+
+if [[ ! -f "$1" ]]
 then
     printf "Invalid playbook (%s)\n" "$1" >&2
     exit 1
 fi
 
+# ------------------------------------------------------------------------------
+# Find commands if they exist
+# ------------------------------------------------------------------------------
+
 yamllint=$(command -v yamllint)
 ansible_playbook=$(command -v ansible-playbook)
 ansible_lint=$(command -v ansible-lint)
 
-if [ -n "${yamllint}" ]
+# ------------------------------------------------------------------------------
+# Create yamllint configuration file, if needed
+# ------------------------------------------------------------------------------
+
+yamllint_conf=.yamllint
+if [[ -n "${yamllint}" && ! -a "${yamllint_conf}" ]]
+then
+    cat >"${yamllint_conf}" <<DONE
+extends: relaxed
+rules:
+  line-length: disable
+  trailing-spaces: disable
+  colons: disable
+  indentation: disable
+  hyphens: disable
+DONE
+fi
+
+# ------------------------------------------------------------------------------
+# Run yamllint if it is installed
+# ------------------------------------------------------------------------------
+
+if [[ -n "${yamllint}" ]]
 then
     printf "Running YAML Lint on %s...\n" "$1"
    "${yamllint}" "$1"                                       \
@@ -27,7 +63,11 @@ else
     printf "yamllint is not installed\n" >&2
 fi
 
-if [ -n "${ansible_playbook}" ]
+# ------------------------------------------------------------------------------
+# Do a syntax check on the playbook
+# ------------------------------------------------------------------------------
+
+if [[ -n "${ansible_playbook}" ]]
 then
     printf "Doing a syntax check on %s...\n" "$1"
     "${ansible_playbook}" --syntax-check "$1"               \
@@ -37,7 +77,11 @@ else
     printf "ansible-playbook is not installed\n" >&2
 fi
 
-if [ -n "${ansible_lint}" ]
+# ------------------------------------------------------------------------------
+# Run Ansible Lint on the playbook
+# ------------------------------------------------------------------------------
+
+if [[ -n "${ansible_lint}" ]]
 then
     printf "Running Ansible Lint on %s...\n" "$1"
     "${ansible_lint}" "$1"                                  \
@@ -46,6 +90,10 @@ then
 else
     printf "ansible-lint is not installed\n" >&2
 fi
+
+# ------------------------------------------------------------------------------
+# Run the playbook in check mode
+# ------------------------------------------------------------------------------
 
 if [ -n "${ansible_playbook}" ]
 then
